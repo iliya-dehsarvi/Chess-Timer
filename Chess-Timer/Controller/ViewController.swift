4400f9af03e77d5f99c5d56cc8af3b2f2c5f17f0
iliya52 <Dehsarvi.iliya@gmail.com>
2021-08-09 23:03:14 -0700

    Update ViewController.swift

--- Chess-Timer/Controller/ViewController.swift
+++ Chess-Timer/Controller/ViewController.swift
@@ -10,11 +10,22 @@ import Charts
 
 class ViewController: UIViewController {
 	
+	let players = ["Ali", "Iliya"]
+	let times = [23, 20]
+	
 	@IBOutlet weak var timerLabel: UILabel!
 	@IBOutlet weak var timerPerMoveChart: LineChartView!
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		var dataEntries: [ChartDataEntry] = []
+		for i in 0..<players.count-1 {
+			let dataEntry = ChartDataEntry(x: Double(times[i]), y: Double(i))
+			dataEntries.append(dataEntry)
+		}
+		let lineChartDataSet = LineChartDataSet(entries: dataEntries, label: nil)
+		let lineChartData = LineChartData(dataSet: lineChartDataSet)
+		timerPerMoveChart.data = lineChartData
 	}
 }
 
