3a59454f462d20bd834c9efcb837dc5596a6e205
iliya52 <Dehsarvi.iliya@gmail.com>
2021-08-10 13:23:32 -0700

    Testing timer

--- Chess-Timer/Controller/ViewController.swift
+++ Chess-Timer/Controller/ViewController.swift
@@ -10,22 +10,73 @@ import Charts
 
 class ViewController: UIViewController {
 	
-	let players = ["Ali", "Iliya"]
-	let times = [23, 20]
-	
 	@IBOutlet weak var timerLabel: UILabel!
-	@IBOutlet weak var timerPerMoveChart: LineChartView!
 	
+	var count = 0
+	var seconds = 9
+	
+	var timer = Timer.scheduledTimer(timeInterval: 0.001, target: self, selector: #selector(update), userInfo: nil, repeats: true)
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		var dataEntries: [ChartDataEntry] = []
-		for i in 0..<players.count-1 {
-			let dataEntry = ChartDataEntry(x: Double(times[i]), y: Double(i))
-			dataEntries.append(dataEntry)
-		}
-		let lineChartDataSet = LineChartDataSet(entries: dataEntries, label: nil)
-		let lineChartData = LineChartData(dataSet: lineChartDataSet)
-		timerPerMoveChart.data = lineChartData
+		timer.invalidate()
 	}
+	
+	@objc func update() {
+//	    if(count > -1) {
+//		    if String(count).count == 1 {
+//			    timerLabel.text = "00:0\(seconds).0\(count)"
+//		    } else {
+//			    timerLabel.text = "00:0\(seconds).\(count)"
+//		    }
+//		    count-=1
+//	    } else if (seconds > 0) {
+//		    if String(count).count == 1 {
+//			    timerLabel.text = "00:0\(seconds).0\(count)"
+//		    } else {
+//			    timerLabel.text = "00:0\(seconds).\(count)"
+//		    }
+//		    count=100
+//		    seconds-=1
+//	    }
+		setTimerLabel(millieSec: count)
+		count += 1
+	}
+	
+	private func setTimerLabel(millieSec: Int) {
+		var min = "\(Int(Double(millieSec)*1.66667e-5))"
+		if min.count == 1 {
+			min = "0\(min)"
+		}
+		
+		var sec = "\(Int((Double(millieSec)*0.0001).truncatingRemainder(dividingBy: 60)*100))"
+		if sec.count == 1 {
+			sec = "0\(sec)"
+		}
+		var millie = String((Int(millieSec%10000)*100)/100)
+		if millie.count == 1 {
+			millie = "0\(millie)"
+		}
+		print("\(min):\(sec).\(millie)")
+		timerLabel.text = "\(min):\(sec).\(millie)"
+	}
+	
+	@IBAction func startButton(_ sender: UIButton) {
+		self.timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(update), userInfo: nil, repeats: true)
+	}
+	
+	@IBAction func resetButton(_ sender: UIButton) {
+		count = 99
+		seconds = 9
+		timerLabel.text = "00:0\(seconds).\(count)"
+		self.timer.invalidate()
+		
+	}
+	@IBAction func pauseButton(_ sender: UIButton) {
+		self.timer.invalidate()
+	}
+	
+	
+	
 }
 
